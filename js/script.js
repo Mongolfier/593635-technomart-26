2e314646110f537777bd9028a6a94c7d3c0774d9
Dmitry <dimony4519@gmail.com>
2019-10-03 22:42:54 +0300

    Еще чуток сетки да украшательств

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-var closeModal = function() {
-  let el = document.querySelector(".write-us");
-  el.classList.add("hide-modal");
+// функция развешивания класса "show-modal" по элементам
+var toggleModal = function(className) {
+  var toggledElem = document.querySelector(className);
+  if( !toggledElem.classList.contains("show-modal") ) {
+    toggledElem.classList.add("show-modal") 
+  } else {
+    toggledElem.classList.remove("show-modal");
+  }
+}
+
+var modalMap = document.getElementsByClassName("toggle-modal-map");
+for( var i = 0; i < modalMap.length; i++) {
+  modalMap[i].addEventListener("click", function(event) {
+    toggleModal(".write-us");
+  });
 }
