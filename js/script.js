f69f8dcc244e2dfba35a2a3f88377f86ebed4257
Dmitry <dimony4519@gmail.com>
2019-10-04 21:25:18 +0300

    вот теперь вот все вот да прям ух даааа

--- js/script.js
+++ js/script.js
@@ -20,7 +20,6 @@ var serviceNav = document.getElementsByClassName("btn-service");
 for( var i = 0; i < serviceNav.length; i++) {
   serviceNav[i].onclick = function(event) {
 
-    console.log(document.querySelectorAll(".service-navigation span").indexOf(event.target)); //!!!!!!!!!!!!!!
     var numberPointNav = document.querySelectorAll(".service-navigation span").length;
     for( var i = 0; i < numberPointNav; i++ ) {
       document.querySelectorAll(".service-navigation span")[i].classList.remove("active");
